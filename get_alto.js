73f2f9f9b7d309fb5c063761878c8b37a2995279
aryawahyu <arya.wahyu502@gmail.com>
2022-07-14 13:22:59 +0700

    update

--- get_alto.js
+++ get_alto.js
@@ -1,31 +1,59 @@
 const readline = require("readline");
 let Client = require("ssh2-sftp-client");
+const moment = require("moment");
+const fs = require("fs");
+
 let sftp = new Client();
+
+const REMOTE_PATH = "/home/ftp/alto-settlement";
+const LOCAL_PATH = "upload";
+const FILENAME = "TELKOM-ALTO_Transfer-Acquirer-Transaction.rpt";
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const getFromSFTP = () => {
+const getFromSFTP = (day) => {
   sftp
     .connect({
       host: "10.1.87.48",
       port: "22",
       username: "alto-settlement",
       password: "4lt0@tmoney.co.id",
+      privateKey: fs.readFileSync("/Users/aryawahyu/.ssh/id_rsa"),
+      readyTimeout: 20000, // integer How long (in ms) to wait for the SSH handshake
+      retries: 2, // integer. Number of times to retry connecting
+      retry_factor: 2, // integer. Time factor used to calculate time between retries
+      retry_minTimeout: 2000, // integer. Minimum timeout between attempts
     })
     .then(() => {
-      console.log("A");
-      //   console.log(sftp.list());
-      //   return sftp.list("/pathname");
+      console.log("Connected to SFTP Server...");
+
+      return sftp.list("/home/ftp/alto-settlement");
+    })
+    .then(async () => {
+      for (let i = day; i >= 1; i--) {
+        const folder_today = moment().subtract(i, "days").format("YYYY-MM-DD");
+        const folder = `${REMOTE_PATH}/${folder_today}`;
+
+        const file_today = moment().subtract(i, "days").format("YY-MM-DD");
+        const file = `${folder}/${file_today}_${FILENAME}`;
+
+        const localfile = `${LOCAL_PATH}/${file_today}_${FILENAME}`;
+
+        await sftp.fastGet(file, localfile);
+
+        console.log(`Sukses Download Data: ${file}`);
+      }
+
+      await sftp.end();
+      console.log("\n");
+      console.log("Closing Connections...");
+    })
+    .catch((err) => {
+      console.log(err, "catch error");
     });
-  // .then((data) => {
-  //   console.log(data, "the data info");
-  // })
-  // .catch((err) => {
-  //   console.log(err, "catch error");
-  // });
 };
 
 const Main = () => {
@@ -44,7 +72,7 @@ const Main = () => {
       if (!isNaN(day)) {
         console.log("Donwload Data from SFTP...");
 
-        getFromSFTP();
+        getFromSFTP(day);
       } else {
         console.log("TRY AGAIN!! \n");
 
